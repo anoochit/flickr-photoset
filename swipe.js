39d06b405367b23fee82a706f15510c761d734be
anoochit <anoochit@8f2be4cb-6586-99cd-1a21-2d2d0874596d>
2012-02-02 12:17:06 +0000

    mod page transition

--- swipe.js
+++ swipe.js
@@ -2,13 +2,13 @@
 			var nextpage = $(this).next('div[data-role="page"]');
 			// swipe using id of next page if exists
 			if (nextpage.length > 0) {
-				$.mobile.changePage(nextpage, 'slide');
+				$.mobile.changePage(nextpage, {transition: 'slide'});
 			}
 		});
 		$('div.ui-page').live("swiperight", function(){
 			var prevpage = $(this).prev('div[data-role="page"]');
 			// swipe using id of next page if exists
 			if (prevpage.length > 0) {
-				$.mobile.changePage(prevpage, 'slide', true);
+				$.mobile.changePage(prevpage, {transition: 'slide', reverse: true});
 			}
 		});
